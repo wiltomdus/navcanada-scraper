af736352a31922570d8c49556e9a2b0a809f82a9
Dhenki <42845952+wiltomdus@users.noreply.github.com>
2024-12-13 20:18:36 -0500

    Support for multiple ICAO codes Added NIGHT data Improved period check

--- app/scraper.py
+++ app/scraper.py
@@ -5,51 +5,52 @@ from time import sleep
 
 import requests
 import schedule
-from pymongo import MongoClient
+import pymongo
 
 # Configuration
-API_URL = "https://plan.navcanada.ca/weather/api/alpha/?site=CYYU&alpha=upperwind"
-
-# Altitude categories
-HIGH_ALTITUDES = {24000, 30000, 34000, 39000, 45000, 53000}
-LOW_ALTITUDES = {3000, 6000, 9000, 12000, 18000}
-
-# MongoDB
+ICAO_CODES = os.getenv("ICAO_CODES", "CYYU").split(",")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/")
 DATABASE_NAME = "navcanada"
 COLLECTION_NAME = "upper_winds"
 
 
-def fetch_upper_winds():
-    """Fetch upper winds data from the Nav Canada API."""
+def fetch_upper_winds(icao_code) -> json:
+    """Fetch upper winds data from the Nav Canada API for a specific ICAO code."""
+    api_url = (
+        f"https://plan.navcanada.ca/weather/api/alpha/?site={icao_code}&alpha=upperwind"
+    )
     try:
-        response = requests.get(API_URL)
+        response = requests.get(api_url, timeout=10)
         response.raise_for_status()
         data = response.json()
         return data
-    except Exception as e:
-        print(f"Error fetching data: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data for {icao_code}: {e}")
         return None
 
 
-def is_am_data(start, end):
-    """Check if the time range falls within the AM period (5am to 9am)."""
+def get_period(start) -> str:
+    """Determine the period (AM, PM, NIGHT) based on the start and end times."""
     start_time = datetime.fromisoformat(start).time()
-    end_time = datetime.fromisoformat(end).time()
-    return time(5, 0) <= start_time <= time(8, 59)
+
+    if time(5, 0) <= start_time < time(9, 0):
+        return "AM"
+    if time(9, 0) <= start_time < time(18, 0):
+        return "PM"
+    if time(18, 0) <= start_time or start_time < time(5, 0):
+        return "NIGHT"
+    return None
 
 
-def is_pm_data(start, end):
-    """Check if the time range falls within the PM period (9am to 18:00)."""
-    start_time = datetime.fromisoformat(start).time()
-    end_time = datetime.fromisoformat(end).time()
-    return time(9, 0) <= start_time <= time(17, 59)
-
-
-def parse_data(data):
-    """Parse and filter data for AM and PM periods, combining altitudes into a single list."""
-    parsed_results = {"AM": [], "PM": []}
-    parsed_results["datetime"] = datetime.now().isoformat()
+def parse_data(data) -> json:
+    """Parse and filter data for AM, PM and NIGHT periods"""
+    parsed_results = {
+        "AM": {"data": [], "startValidity": None, "endValidity": None},
+        "PM": {"data": [], "startValidity": None, "endValidity": None},
+        "NIGHT": {"data": [], "startValidity": None, "endValidity": None},
+        "RAW": {"data": [data]},
+        "datetime": datetime.now().isoformat(),
+    }
 
     for entry in data.get("data", []):
         start_validity = entry["startValidity"]
@@ -58,18 +59,19 @@ def parse_data(data):
         # Extract wind data from the text field
         wind_data = json.loads(entry["text"])[-1]
 
-        # Determine if the entry belongs to AM or PM
-        if is_am_data(start_validity, end_validity):
-            period = "AM"
-        elif is_pm_data(start_validity, end_validity):
-            period = "PM"
-        else:
+        # Determine the period
+        period = get_period(start_validity)
+        if period is None:
             continue
+
+        # Set the start and end validity for the period
+        parsed_results[period]["startValidity"] = start_validity
+        parsed_results[period]["endValidity"] = end_validity
 
         # Combine wind data into a single list for the period
         for item in wind_data:
             altitude, heading, wind, temperature, _ = item
-            parsed_results[period].append(
+            parsed_results[period]["data"].append(
                 {
                     "altitude": altitude,
                     "heading": heading if heading is not None else 0,
@@ -79,39 +81,40 @@ def parse_data(data):
             )
 
     # Sort the results by altitude for each period
-    for period in parsed_results:
-        if period != "datetime":
-            parsed_results[period] = sorted(
-                parsed_results[period], key=lambda x: x["altitude"]
-            )
+    for period in ["AM", "PM", "NIGHT"]:
+        parsed_results[period]["data"] = sorted(
+            parsed_results[period]["data"], key=lambda x: x["altitude"]
+        )
 
     return parsed_results
 
 
-def store_data(data):
+def store_data(data, icao_code) -> None:
     """Store the parsed data into MongoDB."""
     try:
-        client = MongoClient(MONGO_URI)
-        db = client[DATABASE_NAME]
+        client = pymongo.MongoClient(MONGO_URI)
+        db = client[icao_code]  # Use ICAO code as the database name
         collection = db[COLLECTION_NAME]
 
         collection.insert_one(data)
-    except Exception as e:
-        print(f"Error storing data: {e}")
+        print(f"Data for {icao_code} stored in MongoDB.")
+    except pymongo.errors.ConnectionFailure as e:
+        print(f"Connection failure storing data for {icao_code}: {e}")
+    except pymongo.errors.OperationFailure as e:
+        print(f"Operation failure storing data for {icao_code}: {e}")
 
 
 def main():
-
     print("Starting upper winds data scraper...")
-    data = fetch_upper_winds()
-    if data:
-        results = parse_data(data)
-        print(json.dumps(results, indent=4))
+    for icao_code in ICAO_CODES:
+        data = fetch_upper_winds(icao_code)
+        if data:
+            results = parse_data(data)
+            print(json.dumps(results, indent=4))
 
-        store_data(results)
-        print("Data stored in MongoDB.")
-    else:
-        print("No data fetched.")
+            store_data(results, icao_code)
+        else:
+            print(f"No data fetched for {icao_code}.")
 
 
 if __name__ == "__main__":
